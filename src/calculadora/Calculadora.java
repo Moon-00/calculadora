aa66125e700f41598e1b1fb156ccf720dc807a4b
1cfgs <1cfgs@A6PC26>
2021-03-03 11:34:05 +0000

    paso 3 y 4

--- src/calculadora/Calculadora.java
+++ src/calculadora/Calculadora.java
@@ -11,6 +11,33 @@ package calculadora;
  */
 public class Calculadora {
 
+    public string getOperador1() {
+        return operador1;
+    }
+
+    public void setOperador1(string operador1) {
+        this.operador1 = operador1;
+    }
+
+    public string getOperador2() {
+        return operador2;
+    }
+
+    public void setOperador2(string operador2) {
+        this.operador2 = operador2;
+    }
+
+    public string getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(string operacion) {
+        this.operacion = operacion;
+    }
+  private string operador1;
+  private string operador2;
+  private string operacion;
+          
     /**
      * @param args the command line arguments
      */
